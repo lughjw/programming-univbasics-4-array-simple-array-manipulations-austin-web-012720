7ca4ffb6555dd113246f6560303d17ed4b11d28a
Lugh Wilson <lughjw@gmail.com>
2019-12-25 05:48:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,13 @@
-def using_push(array, st)
+def using_push(array, element)
+  array.push(element)
+end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def
